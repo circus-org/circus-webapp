43c1b4cc422881e47e80ff6979aa9d671ee38dea
mniedre <webcane@ya.ru>
2016-03-15 13:43:51 +0300

    #2

--- circus/src/main/java/cane/brothers/circus/domain/Program.java
+++ circus/src/main/java/cane/brothers/circus/domain/Program.java
@@ -4,21 +4,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.joda.time.DateTime;
+import org.springframework.core.style.ToStringCreator;
 
 @Entity
-public class Program {
-
-	//TODO GenerationType
-	
-	@Id
-	@GeneratedValue
-	private Long id;
+public class Program extends BaseEntity {
 	
 	private String title;
 	private String description;
 	
+	// TODO ValueObject for time period
 	// TODO LocalTime
+	
 	// Will be mapped as DATE (on MySQL), i.e. only date without timestamp
 	// For JSON binding use the format: "1970-01-01" (yyyy-MM-dd)
 	//private LocalTime date;
@@ -32,6 +31,117 @@ public class Program {
 	private DateTime endDate;
 	
 	
+	/**
+	 * The troupe of circus performers
+	 */
 	private Troupe actorsGroup;
+
+
+	public Program() {
+	}
+
+
+	public Program(String title, DateTime startDate, DateTime endDate) {
+		super();
+		this.title = title;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
 	
+	public Program(Long id, String title, String description, DateTime startDate, DateTime endDate,
+			Troupe actorsGroup) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.actorsGroup = actorsGroup;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public DateTime getStartDate() {
+		return startDate;
+	}
+
+
+	public void setStartDate(DateTime startDate) {
+		this.startDate = startDate;
+	}
+
+
+	public DateTime getEndDate() {
+		return endDate;
+	}
+
+
+	public void setEndDate(DateTime endDate) {
+		this.endDate = endDate;
+	}
+
+
+	public Troupe getActorsGroup() {
+		return actorsGroup;
+	}
+
+
+	public void setActorsGroup(Troupe actorsGroup) {
+		this.actorsGroup = actorsGroup;
+	}
+	
+	@Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .appendSuper(super.hashCode())
+                .append(this.title)
+                .append(this.startDate)
+                .append(this.endDate)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Program other = (Program) obj;
+        return new EqualsBuilder().appendSuper(super.equals(obj))
+                .append(this.title, other.title)
+                .append(this.startDate, other.startDate)
+                .append(this.endDate, other.endDate)
+                .isEquals();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringCreator(this)
+                .append("id", this.getId())
+                .append("title", this.title)
+                .append("startDate", this.startDate)
+                .append("endDate", this.endDate)
+                .append("actorsGroup", this.actorsGroup)
+                .toString();
+    }
 }
--- circus/src/main/java/cane/brothers/circus/domain/Troupe.java
+++ circus/src/main/java/cane/brothers/circus/domain/Troupe.java
@@ -1,19 +1,24 @@
 package cane.brothers.circus.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.springframework.core.style.ToStringCreator;
 
 @Entity
-public class Troupe {
-	
-	@Id
-	@GeneratedValue
-	private Long id;
+public class Troupe extends BaseEntity {
 
+	/**
+	 * brand name of the troupe.
+	 */
 	private String name;
+
+	/**
+	 * Head of the troupe - responsible person.
+	 */
 	private String head;
-	
+
 	public Troupe() {
 	}
 
@@ -36,9 +41,40 @@ public class Troupe {
 	public void setHead(String head) {
 		this.head = head;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+				.appendSuper(super.hashCode())
+				.append(this.name)
+				.append(this.head)
+				.toHashCode();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Troupe other = (Troupe) obj;
+		return new EqualsBuilder()
+				.appendSuper(super.equals(obj))
+				.append(this.name, other.name)
+				.append(this.head, other.head)
+				.isEquals();
+	}
+
 	@Override
 	public String toString() {
-		return String.format("Troupe [id=%s, name=%s, head=%s]", id, name, head);
+		// return String.format("Troupe [id=%s, name=%s, head=%s]", id, name,
+		// head);
+		return new ToStringCreator(this)
+				.append("id", this.getId())
+				.append("name", this.name)
+				.append("head", this.head)
+				.toString();
 	}
 }

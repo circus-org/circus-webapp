74fb1f51a026002615a0276abdb8bccc958cd1f9
cane <webcane@ya.ru>
2016-03-18 23:26:14 +0300

    test works

--- circus/src/test/java/cane/brothers/circus/TestTroupeRepository.java
+++ circus/src/test/java/cane/brothers/circus/TestTroupeRepository.java
@@ -1,23 +1,28 @@
 package cane.brothers.circus;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import javax.annotation.Resource;
 
-import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import cane.brothers.circus.domain.Troupe;
 import cane.brothers.circus.repository.TroupeRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(classes = JpaConfig.class)
 public class TestTroupeRepository {
 
 	@Resource
 	private TroupeRepository troupeRepo;
 	
-	@Before
+	@Test
 	public void createTroupes() {
-		// TODO
+		Troupe liones = troupeRepo.save(new Troupe("Канские Львы", "Михаилков"));
+		assertThat(troupeRepo.findOne(liones.getId()), is(liones));
 	}
 }

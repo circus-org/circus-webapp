c760d7b1ad7be3b357cd62711c5c17ca48e73ab3
cane <webcane@ya.ru>
2016-03-24 00:11:28 +0300

    8: create Ticket entity Task-Url: https://github.com/circus-org/circus-webapp/issues/issue/8

---
+++ circus/src/main/java/cane/brothers/circus/domain/Ticket.java
@@ -0,0 +1,23 @@
+package cane.brothers.circus.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TICKETS")
+public class Ticket extends BaseEntity {
+
+	/**
+	 * Код сеанса
+	 */
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "PERFORMANCE_ID", unique = true, nullable = false)
+	private Performance performance;
+	
+	public Ticket() {
+	}
+
+}

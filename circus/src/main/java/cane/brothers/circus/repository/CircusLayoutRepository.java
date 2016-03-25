c03b961bebf10905d581bea76a63fb72fb70345e
cane <webcane@ya.ru>
2016-03-25 23:01:44 +0300

    add circus repos

---
+++ circus/src/main/java/cane/brothers/circus/repository/CircusLayoutRepository.java
@@ -0,0 +1,9 @@
+package cane.brothers.circus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cane.brothers.circus.domain.CircusLayout;
+
+public interface CircusLayoutRepository extends JpaRepository<CircusLayout, Long> {
+
+}
